99fd385dcc760786e10bf5e4578c248f2cf0e578
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-18 21:39:17 +0000

    Move Vec3 code to correct file. Update target in Package.swift.

--- Package.swift
+++ Package.swift
@@ -4,11 +4,13 @@ import PackageDescription
  
 let package = Package(
     name: "Swift-Math",
+    platforms: [.iOS("15.2")],
     products: [
         .library(
             name: "Swift-Math",
             targets: ["Swift-Math"]),
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "Swift-Math",
