ceebf5356ca7d261883525785703c38b0c2a2bf0
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-18 21:57:01 +0000

    Add Vec3 Test

---
+++ Tests/Swift-MathTests/Vec3Tests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import Swift_Math
+ 
+class Vec3Tests: XCTestCase {
+ 
+    override func setUpWithError() throws {}
+    }
+ 
+    override func tearDownWithError() throws {
+    }
+ 
+    func trueTest() throws {
+        XCTAssert(true, "Something that's true")
+    }
+
+    func falseTest() throws {
+        XCTAssert(false, "Something that's false")
+    }
+}
