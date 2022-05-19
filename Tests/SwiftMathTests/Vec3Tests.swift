2b186690ba9fa6ab64bbf311ffea6604f87584d5
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-19 08:59:02 +0100

    rename tests

--- Tests/SwiftMathTests/Vec3Tests.swift
+++ Tests/SwiftMathTests/Vec3Tests.swift
@@ -1,19 +1,19 @@
 import XCTest
 @testable import Swift_Math
- 
+
 class Vec3Tests: XCTestCase {
- 
+
     override func setUpWithError() throws {
     }
- 
+
     override func tearDownWithError() throws {
     }
- 
-    func trueTest() throws {
+
+    func testTrue() throws {
         XCTAssert(true, "Something that's true")
     }
 
-    func falseTest() throws {
+    func testFalse() throws {
         XCTAssert(false, "Something that's false")
     }
 }
