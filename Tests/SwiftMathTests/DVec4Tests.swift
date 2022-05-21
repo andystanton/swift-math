d2917cfd375c7113108b31c97bdf068a7362d75e
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-21 17:54:03 +0100

    Add some vec4 tests

--- Tests/SwiftMathTests/DVec4Tests.swift
+++ Tests/SwiftMathTests/DVec4Tests.swift
@@ -9,4 +9,26 @@ class DVec4Tests: XCTestCase {
        let actual = [candidate.x, candidate.y, candidate.z, candidate.w]
        XCTAssertEqual(actual, expected, "Init failed")
     }
+
+    func testAxialUnitConstants() {
+        let expected: [[Double]] = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+        let actual = [
+            DVec4.xUnit,
+            DVec4.yUnit,
+            DVec4.zUnit,
+            DVec4.wUnit,
+        ].map { v in [v.x, v.y, v.z, v.w]}
+        XCTAssertEqual(actual, expected, "Axial Unit Constants failed")
+    }
+
+    func testSubvector() {
+        let expected = DVec3(x: 1.0, y: 2.0, z: 3.0)
+        let actual = DVec4(1.0, 2.0, 3.0, 4.0).xyz
+        XCTAssertEqual(actual, expected, "Subvector failed")
+    }
 }
