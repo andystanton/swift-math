b62e762cbc1d255409e7477132b39d7a0374a25e
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-21 23:12:49 +0100

    try adding xctest dep

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "SwiftMathTests",
-            dependencies: ["SwiftMath"]),
+            dependencies: ["XCTest", "SwiftMath"]),
     ]
 )
