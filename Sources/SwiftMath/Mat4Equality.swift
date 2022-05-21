41c8f36af4241977fdf10f120e001b953c4b493d
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-21 23:37:45 +0100

    forward in time

--- Sources/SwiftMath/Mat4Equality.swift
+++ Sources/SwiftMath/Mat4Equality.swift
@@ -10,6 +10,28 @@ extension Mat4 {
             && lhs[3][3] == rhs[3][3]
     }
 
+    public func almostEquals(_ rhs: Mat4<T>) -> Bool where T == Float {
+        return self[0][0].almostEquals(rhs[0][0]) && self[0][1].almostEquals(rhs[0][1])
+            && self[0][2].almostEquals(rhs[0][2]) && self[0][3].almostEquals(rhs[0][3])
+            && self[1][0].almostEquals(rhs[1][0]) && self[1][1].almostEquals(rhs[1][1])
+            && self[1][2].almostEquals(rhs[1][2]) && self[1][3].almostEquals(rhs[1][3])
+            && self[2][0].almostEquals(rhs[2][0]) && self[2][1].almostEquals(rhs[2][1])
+            && self[2][2].almostEquals(rhs[2][2]) && self[2][3].almostEquals(rhs[2][3])
+            && self[3][0].almostEquals(rhs[3][0]) && self[3][1].almostEquals(rhs[3][1])
+            && self[3][2].almostEquals(rhs[3][2]) && self[3][3].almostEquals(rhs[3][3])
+    }
+
+    public func almostEquals(_ rhs: Mat4<T>) -> Bool where T == Double {
+        return self[0][0].almostEquals(rhs[0][0]) && self[0][1].almostEquals(rhs[0][1])
+            && self[0][2].almostEquals(rhs[0][2]) && self[0][3].almostEquals(rhs[0][3])
+            && self[1][0].almostEquals(rhs[1][0]) && self[1][1].almostEquals(rhs[1][1])
+            && self[1][2].almostEquals(rhs[1][2]) && self[1][3].almostEquals(rhs[1][3])
+            && self[2][0].almostEquals(rhs[2][0]) && self[2][1].almostEquals(rhs[2][1])
+            && self[2][2].almostEquals(rhs[2][2]) && self[2][3].almostEquals(rhs[2][3])
+            && self[3][0].almostEquals(rhs[3][0]) && self[3][1].almostEquals(rhs[3][1])
+            && self[3][2].almostEquals(rhs[3][2]) && self[3][3].almostEquals(rhs[3][3])
+    }
+
     public static func ~= (lhs: Mat4<T>, rhs: Mat4<T>) -> Bool where T == Double {
         return lhs[0][0].almostEquals(rhs[0][0]) && lhs[0][1].almostEquals(rhs[0][1])
             && lhs[0][2].almostEquals(rhs[0][2]) && lhs[0][3].almostEquals(rhs[0][3])
--- Sources/SwiftMath/Mat4Multiplication.swift
+++ Sources/SwiftMath/Mat4Multiplication.swift
@@ -1,43 +1,63 @@
-// public extension Mat4 {
-//     static func * (lhs: Mat4<T>, rhs: Vec4<T>) -> Vec4<T> {
-//         return Vec4<T>(
-//             lhs[0][0] * rhs.x + lhs[1][0] * rhs.y + lhs[2][0] * rhs.z + lhs[3][0] * rhs.w,
-//             lhs[0][1] * rhs.x + lhs[1][1] * rhs.y + lhs[2][1] * rhs.z + lhs[3][1] * rhs.w,
-//             lhs[0][2] * rhs.x + lhs[1][2] * rhs.y + lhs[2][2] * rhs.z + lhs[3][2] * rhs.w,
-//             lhs[0][3] * rhs.x + lhs[1][3] * rhs.y + lhs[2][3] * rhs.z + lhs[3][3] * rhs.w)
-//     }
+extension Mat4 {
+    public static func * (lhs: Mat4<T>, rhs: Vec4<T>) -> Vec4<T> {
+        return Vec4<T>(
+            lhs[0][0] * rhs.x + lhs[1][0] * rhs.y + lhs[2][0] * rhs.z + lhs[3][0] * rhs.w,
+            lhs[0][1] * rhs.x + lhs[1][1] * rhs.y + lhs[2][1] * rhs.z + lhs[3][1] * rhs.w,
+            lhs[0][2] * rhs.x + lhs[1][2] * rhs.y + lhs[2][2] * rhs.z + lhs[3][2] * rhs.w,
+            lhs[0][3] * rhs.x + lhs[1][3] * rhs.y + lhs[2][3] * rhs.z + lhs[3][3] * rhs.w)
+    }
 
-//     static func * (lhs: Mat4<T>, rhs: Mat4<T>) -> Mat4<T> {
-//         let col1 =
-//         [
-//             lhs.data[0][0] * rhs.data[0][0] + lhs.data[0][1] * rhs.data[1][0] + lhs.data[0][2] * rhs.data[2][0] + lhs.data[0][3] * rhs.data[3][0],
-//             lhs.data[0][0] * rhs.data[0][1] + lhs.data[0][1] * rhs.data[1][1] + lhs.data[0][2] * rhs.data[2][1] + lhs.data[0][3] * rhs.data[3][1],
-//             lhs.data[0][0] * rhs.data[0][2] + lhs.data[0][1] * rhs.data[1][2] + lhs.data[0][2] * rhs.data[2][2] + lhs.data[0][3] * rhs.data[3][2],
-//             lhs.data[0][0] * rhs.data[0][3] + lhs.data[0][1] * rhs.data[1][3] + lhs.data[0][2] * rhs.data[2][3] + lhs.data[0][3] * rhs.data[3][3],
-//         ]
-//         let col2 =
-//         [
-//             lhs.data[1][0] * rhs.data[0][0] + lhs.data[1][1] * rhs.data[1][0] + lhs.data[1][2] * rhs.data[2][0] + lhs.data[1][3] * rhs.data[3][0],
-//             lhs.data[1][0] * rhs.data[0][1] + lhs.data[1][1] * rhs.data[1][1] + lhs.data[1][2] * rhs.data[2][1] + lhs.data[1][3] * rhs.data[3][1],
-//             lhs.data[1][0] * rhs.data[0][2] + lhs.data[1][1] * rhs.data[1][2] + lhs.data[1][2] * rhs.data[2][2] + lhs.data[1][3] * rhs.data[3][2],
-//             lhs.data[1][0] * rhs.data[0][3] + lhs.data[1][1] * rhs.data[1][3] + lhs.data[1][2] * rhs.data[2][3] + lhs.data[1][3] * rhs.data[3][3],
-//         ]
-//         let col3 =
-//         [
-//             lhs.data[2][0] * rhs.data[0][0] + lhs.data[2][1] * rhs.data[1][0] + lhs.data[2][2] * rhs.data[2][0] + lhs.data[2][3] * rhs.data[3][0],
-//             lhs.data[2][0] * rhs.data[0][1] + lhs.data[2][1] * rhs.data[1][1] + lhs.data[2][2] * rhs.data[2][1] + lhs.data[2][3] * rhs.data[3][1],
-//             lhs.data[2][0] * rhs.data[0][2] + lhs.data[2][1] * rhs.data[1][2] + lhs.data[2][2] * rhs.data[2][2] + lhs.data[2][3] * rhs.data[3][2],
-//             lhs.data[2][0] * rhs.data[0][3] + lhs.data[2][1] * rhs.data[1][3] + lhs.data[2][2] * rhs.data[2][3] + lhs.data[2][3] * rhs.data[3][3],
-//         ]
-//         let col4 =
-//         [
-//             lhs.data[3][0] * rhs.data[0][0] + lhs.data[3][1] * rhs.data[1][0] + lhs.data[3][2] * rhs.data[2][0] + lhs.data[3][3] * rhs.data[3][0],
-//             lhs.data[3][0] * rhs.data[0][1] + lhs.data[3][1] * rhs.data[1][1] + lhs.data[3][2] * rhs.data[2][1] + lhs.data[3][3] * rhs.data[3][1],
-//             lhs.data[3][0] * rhs.data[0][2] + lhs.data[3][1] * rhs.data[1][2] + lhs.data[3][2] * rhs.data[2][2] + lhs.data[3][3] * rhs.data[3][2],
-//             lhs.data[3][0] * rhs.data[0][3] + lhs.data[3][1] * rhs.data[1][3] + lhs.data[3][2] * rhs.data[2][3] + lhs.data[3][3] * rhs.data[3][3],
-//         ]
-//         return Mat4<T>(data: [
-//             col1, col2, col3, col4
-//         ])
-//     }
-// }
+    public static func * (lhs: Mat4<T>, rhs: Mat4<T>) -> Mat4<T> {
+        assert(lhs[0].count == 4)
+        assert(lhs[1].count == 4)
+        assert(lhs[2].count == 4)
+        assert(lhs[3].count == 4)
+        let col1: [T] =
+            [
+                lhs.data[0][0] * rhs.data[0][0] + lhs.data[0][1] * rhs.data[1][0] + lhs.data[0][2]
+                    * rhs.data[2][0] + lhs.data[0][3] * rhs.data[3][0],
+                lhs.data[0][0] * rhs.data[0][1] + lhs.data[0][1] * rhs.data[1][1] + lhs.data[0][2]
+                    * rhs.data[2][1] + lhs.data[0][3] * rhs.data[3][1],
+                lhs.data[0][0] * rhs.data[0][2] + lhs.data[0][1] * rhs.data[1][2] + lhs.data[0][2]
+                    * rhs.data[2][2] + lhs.data[0][3] * rhs.data[3][2],
+                lhs.data[0][0] * rhs.data[0][3] + lhs.data[0][1] * rhs.data[1][3] + lhs.data[0][2]
+                    * rhs.data[2][3] + lhs.data[0][3] * rhs.data[3][3],
+            ]
+        let col2: [T] =
+            [
+                lhs.data[1][0] * rhs.data[0][0] + lhs.data[1][1] * rhs.data[1][0] + lhs.data[1][2]
+                    * rhs.data[2][0] + lhs.data[1][3] * rhs.data[3][0],
+                lhs.data[1][0] * rhs.data[0][1] + lhs.data[1][1] * rhs.data[1][1] + lhs.data[1][2]
+                    * rhs.data[2][1] + lhs.data[1][3] * rhs.data[3][1],
+                lhs.data[1][0] * rhs.data[0][2] + lhs.data[1][1] * rhs.data[1][2] + lhs.data[1][2]
+                    * rhs.data[2][2] + lhs.data[1][3] * rhs.data[3][2],
+                lhs.data[1][0] * rhs.data[0][3] + lhs.data[1][1] * rhs.data[1][3] + lhs.data[1][2]
+                    * rhs.data[2][3] + lhs.data[1][3] * rhs.data[3][3],
+            ]
+        let col3: [T] =
+            [
+                lhs.data[2][0] * rhs.data[0][0] + lhs.data[2][1] * rhs.data[1][0] + lhs.data[2][2]
+                    * rhs.data[2][0] + lhs.data[2][3] * rhs.data[3][0],
+                lhs.data[2][0] * rhs.data[0][1] + lhs.data[2][1] * rhs.data[1][1] + lhs.data[2][2]
+                    * rhs.data[2][1] + lhs.data[2][3] * rhs.data[3][1],
+                lhs.data[2][0] * rhs.data[0][2] + lhs.data[2][1] * rhs.data[1][2] + lhs.data[2][2]
+                    * rhs.data[2][2] + lhs.data[2][3] * rhs.data[3][2],
+                lhs.data[2][0] * rhs.data[0][3] + lhs.data[2][1] * rhs.data[1][3] + lhs.data[2][2]
+                    * rhs.data[2][3] + lhs.data[2][3] * rhs.data[3][3],
+            ]
+        let col4: [T] =
+            [
+                lhs.data[3][0] * rhs.data[0][0] + lhs.data[3][1] * rhs.data[1][0] + lhs.data[3][2]
+                    * rhs.data[2][0] + lhs.data[3][3] * rhs.data[3][0],
+                lhs.data[3][0] * rhs.data[0][1] + lhs.data[3][1] * rhs.data[1][1] + lhs.data[3][2]
+                    * rhs.data[2][1] + lhs.data[3][3] * rhs.data[3][1],
+                lhs.data[3][0] * rhs.data[0][2] + lhs.data[3][1] * rhs.data[1][2] + lhs.data[3][2]
+                    * rhs.data[2][2] + lhs.data[3][3] * rhs.data[3][2],
+                lhs.data[3][0] * rhs.data[0][3] + lhs.data[3][1] * rhs.data[1][3] + lhs.data[3][2]
+                    * rhs.data[2][3] + lhs.data[3][3] * rhs.data[3][3],
+            ]
+        return Mat4<T>(data: [
+            col1, col2, col3, col4,
+        ])
+    }
+}
