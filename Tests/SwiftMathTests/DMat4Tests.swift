cd7d0b002d066e44d71ff1dc150f1c44dee7c7ea
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-21 18:10:23 +0100

    Add vec4 and mat4 tests

---
+++ Tests/SwiftMathTests/DMat4Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftMath
+
+class DMat4Tests: XCTestCase {
+    func testInit() {
+       let expected: [Double] = [1, 2, 3, 4]
+       let candidate = DVec4(x: 1, y: 2, z: 3, w: 4)
+       let actual = [candidate.x, candidate.y, candidate.z, candidate.w]
+       XCTAssertEqual(actual, expected, "Init failed")
+    }
+}
