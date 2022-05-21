d29d5c73bb9359aa641962151c7d491f03c69cc5
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-21 10:20:42 +0100

    rename vec3tests to dvec3tests since there will probably be quite a lot
    add dot and cross product tests

--- Tests/SwiftMathTests/DVec3Tests.swift
+++ Tests/SwiftMathTests/DVec3Tests.swift
@@ -1,30 +1,19 @@
 import XCTest
 @testable import SwiftMath
 
-protocol AlmostEquatable {
-    func almostEquals(rhs: Self)
-}
-
-class Vec3Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-    }
-
-    override func tearDownWithError() throws {
-    }
-
-    func testDVec3Init() {
+class DVec3Tests: XCTestCase {
+    func testInit() {
        let expected: [Double] = [1, 2, 3]
        let candidate = DVec3(x: 1, y: 2, z: 3)
        let actual = [candidate.x, candidate.y, candidate.z]
        XCTAssertEqual(actual, expected, "Init failed")
     }
 
-    func testDVec3AlmostEqual() {
+    func testAlmostEqual() {
         XCTAssertTrue(DVec3(1.231999999).almostEquals(DVec3(1.232)), "Almost Equal failed")
     }
 
-    func testDVec3AxialUnitConstants() {
+    func testAxialUnitConstants() {
         let expected: [[Double]] = [
             [1, 0, 0],
             [0, 1, 0],
@@ -38,59 +27,107 @@ class Vec3Tests: XCTestCase {
         XCTAssertEqual(actual, expected, "Axial Units failed")
     }
 
-    func testDVec3Length() {
+    func testLength() {
         let expected: Double = 7
         let candidate = DVec3(x: 2, y: 3, z: 6)
         let actual = candidate.len()
         XCTAssertEqual(actual, expected, "Length failed")
     }
 
-    func testDVec3Normalise() {
+    func testNormalise() {
         let expected = DVec3((1/3).squareRoot())
         let candidate = DVec3(x: 1, y: 1, z: 1)
         let actual = candidate.normalize()
         XCTAssertTrue(actual.almostEquals(expected), "Normalise failed")
     }
 
-    func testDVec3UniformScale() {
+    func testUniformScale() {
         let expected = DVec3(x: 2, y: 4, z: 6)
         let candidate = DVec3(x: 1, y: 2, z: 3)
         let actual = candidate * 2
         XCTAssertTrue(actual.almostEquals(expected), "Uniform Scale failed")
     }
 
-    func testDVec3NonUniformScale() {
+    func testNonUniformScale() {
         let expected = DVec3(x: 2, y: 6, z: 12)
         let candidate = DVec3(x: 1, y: 2, z: 3)
         let actual = candidate * DVec3(x: 2, y: 3, z: 4)
         XCTAssertTrue(actual.almostEquals(expected), "Non-Uniform Scale failed")
     }
 
-    func testDVec3UniformDivison() {
+    func testUniformDivison() {
         let expected = DVec3(x: 1, y: 2, z: 3)
         let candidate = DVec3(x: 2, y: 4, z: 6)
         let actual = candidate / 2
         XCTAssertEqual(actual, expected, "Uniform Division failed")
     }
 
-    func testDVec3NonUniformDivison() {
+    func testNonUniformDivison() {
         let expected = DVec3(x: 1, y: 2, z: 3)
         let candidate = DVec3(x: 2, y: 6, z: 12)
         let actual = candidate / DVec3(x: 2, y: 3, z: 4)
         XCTAssertEqual(actual, expected, "Non-Uniform Division failed")
     }
 
-    func testDVec3Addition() {
+    func testAddition() {
         let expected = DVec3(x: 3, y: 5, z: 7)
         let candidate = DVec3(x: 1, y: 2, z: 3)
         let actual = candidate + DVec3(x: 2, y: 3, z: 4)
         XCTAssertEqual(actual, expected, "Addition failed")
     }
 
-    func testDVec3Subtraction() {
+    func testSubtraction() {
         let expected = DVec3(x: 1, y: 2, z: 3)
         let candidate = DVec3(x: 3, y: 5, z: 7)
         let actual = candidate - DVec3(x: 2, y: 3, z: 4)
         XCTAssertEqual(actual, expected, "Subtraction failed")
+    }
+
+    func testDotProduct() {
+        let candidate = DVec3(x: 1, y: 0, z: 0)
+        let testValues = [
+            DVec3(x: 1, y: 0, z: 0),
+            DVec3(x: -1, y: 0, z: 0),
+            DVec3(x: 0, y: 1, z: 0),
+            DVec3(x: 0, y: -1, z: 0),
+            DVec3(x: 0, y: 0, z: 1),
+            DVec3(x: 0, y: 0, z: -1),
+            DVec3(x: 0.5, y: 0.8660254038, z: 0),
+            DVec3(x: -0.5, y: -0.8660254038, z: 0),
+        ]
+        let expected = [
+            1,
+            -1,
+            0,
+            0,
+            0,
+            0,
+            0.5,
+            -0.5,
+        ]
+        let actual = testValues.map(candidate.dot)
+        XCTAssertEqual(actual, expected, "Dot Product failed")
+    }
+
+    func testCrossProduct() {
+        let candidate = DVec3(x: 1, y: 0, z: 0)
+        let testValues = [
+            DVec3(x: 1, y: 0, z: 0),
+            DVec3(x: -1, y: 0, z: 0),
+            DVec3(x: 0, y: 1, z: 0),
+            DVec3(x: 0, y: -1, z: 0),
+            DVec3(x: 0, y: 0, z: 1),
+            DVec3(x: 0, y: 0, z: -1),
+        ]
+        let expected = [
+            DVec3(x: 0, y: 0, z: 0),
+            DVec3(x: 0, y: 0, z: 0),
+            DVec3(x: 0, y: 0, z: 1),
+            DVec3(x: 0, y: 0, z: -1),
+            DVec3(x: 0, y: -1, z: 0),
+            DVec3(x: 0, y: 1, z: 0),
+        ]
+        let actual = testValues.map(candidate.cross)
+        XCTAssertEqual(actual, expected, "Cross Product failed")
     }
 }
