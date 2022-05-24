2d64366b29a368365716b1737f1e552a24fc674b
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-24 22:29:02 +0100

    make mat4 methods public

--- Sources/SwiftMath/Mat4.swift
+++ Sources/SwiftMath/Mat4.swift
@@ -6,13 +6,13 @@ typealias FMat4 = Mat4<Float>
 public struct Mat4<T: FloatingPoint> {
     let data: [[T]]
 
-    subscript(index: Int) -> [T] {
+    public subscript(index: Int) -> [T] {
         get {
             return data[index]
         }
     }
 
-    func transpose() -> Mat4<T> {
+    public func transpose() -> Mat4<T> {
         return Mat4(data: [
             [self[0][0], self[1][0], self[2][0], self[3][0]],
             [self[0][1], self[1][1], self[2][1], self[3][1]],
@@ -21,7 +21,7 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    static func translation(_ tVec: Vec3<T>) -> Mat4<T> {
+    public static func translation(_ tVec: Vec3<T>) -> Mat4<T> {
         let identity = Mat4(1)
         return Mat4(data: [
             identity[0],
@@ -31,11 +31,11 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    static func rotation(_ rVec: Vec3<T>) -> Mat4<T> {
+    public static func rotation(_ rVec: Vec3<T>) -> Mat4<T> {
         return Mat4(1)
     }
 
-    static func rotation(_ rVec: Vec3<T>) -> Mat4<T> where T == Double {
+    public static func rotation(_ rVec: Vec3<T>) -> Mat4<T> where T == Double {
         let col1 = [cos(rVec.y) * cos(rVec.z), cos(rVec.y) * sin(rVec.z), -sin(rVec.y), 0]
         let col2 = [
             sin(rVec.x) * sin(rVec.y) * cos(rVec.z) - cos(rVec.x) * sin(rVec.z),
@@ -55,7 +55,7 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    static func rotation(_ rVec: Vec3<T>) -> Mat4<T> where T == Float {
+    public static func rotation(_ rVec: Vec3<T>) -> Mat4<T> where T == Float {
         let col1 = [cos(rVec.y) * cos(rVec.z), cos(rVec.y) * sin(rVec.z), -sin(rVec.y), 0]
         let col2 = [
             sin(rVec.x) * sin(rVec.y) * cos(rVec.z) - cos(rVec.x) * sin(rVec.z),
@@ -75,7 +75,7 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    static func scale(_ rVec: Vec3<T>) -> Mat4<T> where T == Double {
+    public static func scale(_ rVec: Vec3<T>) -> Mat4<T> where T == Double {
         return Mat4(data: [
             [rVec.x, 0, 0, 0],
             [0, rVec.y, 0, 0],
@@ -84,7 +84,7 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    static func scale(_ rVec: Vec3<T>) -> Mat4<T> where T == Float {
+    public static func scale(_ rVec: Vec3<T>) -> Mat4<T> where T == Float {
         return Mat4(data: [
             [rVec.x, 0, 0, 0],
             [0, rVec.y, 0, 0],
@@ -93,8 +93,8 @@ public struct Mat4<T: FloatingPoint> {
         ])
     }
 
-    func flatten() -> [T] {
-        return Array(self.data[0] + self.data[1] + self.data[2] + self.data[3])
+    public func flatten() -> [T] {
+        return self.data[0] + self.data[1] + self.data[2] + self.data[3]
     }
 }
 
