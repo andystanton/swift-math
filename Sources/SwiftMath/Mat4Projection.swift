cf1909de4abcb8ddc9bd565d64af6335999d57ba
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-24 07:42:18 +0000

    fix typo

--- Sources/SwiftMath/Mat4Projection.swift
+++ Sources/SwiftMath/Mat4Projection.swift
@@ -18,7 +18,7 @@ extension Mat4 {
         ])
     }
 
-j   static func perspective(fovY: T, aspect: T, near: T, far: T) -> Mat4<T> {
+    static func perspective(fovY: T, aspect: T, near: T, far: T) -> Mat4<T> {
         let col1: [T] = [2 * near / (right - left), 0, 0, 0]
         let col2: [T] = [0, 2 * near / (top - bottom), 0, 0]
         let col3: [T] = [0, 0, -(far + near) / (far - near), -1]
