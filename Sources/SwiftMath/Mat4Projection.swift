25db4a45f6c29e14b3d75f76f30cffccc4d77219
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-24 08:15:38 +0000

    perspective -> frustum
    add perspective

--- Sources/SwiftMath/Mat4Projection.swift
+++ Sources/SwiftMath/Mat4Projection.swift
@@ -18,11 +18,25 @@ extension Mat4 {
         ])
     }
 
-    static func perspective(fovY: T, aspect: T, near: T, far: T) -> Mat4<T> {
+    static func frustum(left: T, right: T, bottom: T, top: T, near: T, far: T) -> Mat4<T> where T: FloatingPoint
         let col1: [T] = [2 * near / (right - left), 0, 0, 0]
         let col2: [T] = [0, 2 * near / (top - bottom), 0, 0]
         let col3: [T] = [0, 0, -(far + near) / (far - near), -1]
         let col4: [T] = -near * (right + left) / (right - left), -near * (top + bottom) / (top - bottom), 2 * far * near / (near - far), 0]
+        return Mat4(data: [
+            col1,
+            col2,
+            col3,
+            col4,
+        ])
+    }
+
+    static func perspective(fov: T, aspect: T, near: T, far: T) -> Mat4<T> where T: FloatingPoint {
+        let tanHalfFov = tanf(fov / 2)
+        let col1 = [1 / (tanHalfFov * aspect), 0, 0, 0]
+        let col2 = [0, 1 / tanHalfFov, 0, 0]
+        let col3 = [0, 0, (-near - far) / (near - far), 2 * far * near / (near - far)]
+        let col4 = [0, 0, 1, 0]
         return Mat4(data: [
             col1,
             col2,
