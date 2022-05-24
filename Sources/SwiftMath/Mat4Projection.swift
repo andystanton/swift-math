2410296085650a011b764b57ecaa595272aa8611
Andy Stanton <andystanton@users.noreply.github.com>
2022-05-24 07:35:05 +0000

    add perspective matrix

--- Sources/SwiftMath/Mat4Projection.swift
+++ Sources/SwiftMath/Mat4Projection.swift
@@ -18,7 +18,16 @@ extension Mat4 {
         ])
     }
 
-    static func perspective(fovY: T, aspect: T, near: T, far: T) -> Mat4<T> {
-        return Mat4(1)
+j   static func perspective(fovY: T, aspect: T, near: T, far: T) -> Mat4<T> {
+        let col1: [T] = [2 * near / (right - left), 0, 0, 0]
+        let col2: [T] = [0, 2 * near / (top - bottom), 0, 0]
+        let col3: [T] = [0, 0, -(far + near) / (far - near), -1]
+        let col4: [T] = -near * (right + left) / (right - left), -near * (top + bottom) / (top - bottom), 2 * far * near / (near - far), 0]
+        return Mat4(data: [
+            col1,
+            col2,
+            col3,
+            col4,
+        ])
     }
 }
