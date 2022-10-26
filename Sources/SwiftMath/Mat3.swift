6dc3be5ad741fc2b55c0b8978be66eee12755d0f
Andy Stanton <andystanton@users.noreply.github.com>
2022-10-26 23:43:32 +0100

    add toMat4

--- Sources/SwiftMath/Mat3.swift
+++ Sources/SwiftMath/Mat3.swift
@@ -17,22 +17,30 @@ extension simd_float3x3 {
                 && columns.2.almostEquals(other.columns.2)
     }
 
-    public static func scale(_ scalar: Float) -> FMat3 {
-        FMat3(
+    public func toMat4() -> simd_float4x4 {
+        simd_float4x4(
+                columns.0.toDir(),
+                columns.1.toDir(),
+                columns.2.toDir(),
+                FVec4.wUnit)
+    }
+
+    public static func scale(_ scalar: Float) -> Self {
+        Self(
                 FVec3(scalar, 0, 0),
                 FVec3(0, scalar, 0),
                 FVec3(0, 0, scalar))
     }
 
-    public static func scale(_ scaleVector: FVec2) -> FMat3 {
-        FMat3(
+    public static func scale(_ scaleVector: FVec2) -> Self {
+        Self(
                 FVec3(scaleVector.x, 0, 0),
                 FVec3(0, scaleVector.y, 0),
                 FVec3(0, 0, 1))
     }
 
-    public static func translation(_ translateVector: FVec2) -> FMat3 {
-        FMat3(
+    public static func translation(_ translateVector: FVec2) -> Self {
+        Self(
                 FVec3(1, 0, 0),
                 FVec3(0, 1, 0),
                 FVec3(translateVector, 1))
